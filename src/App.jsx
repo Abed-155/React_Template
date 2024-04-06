bb06ec5306ccabedc46c44dc0025bd204b3d4445
Abed Ali <mdabedali15@gmail.com>
2024-04-06 12:37:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="bg-orange-600 text-white text-center py-4 text-3xl">
+        Welcome to React + Vite
+      </h1>
+    </div>
+  );
+}
